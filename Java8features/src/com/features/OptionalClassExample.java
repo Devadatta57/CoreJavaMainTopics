5abde5159c72a51fd96fae3456c7ea18c79f1fdf
Devadatta57 <193812273+Devadatta57@users.noreply.github.com>
2026-02-03 16:04:57 +0530

    Uncomment Optional class example code
    Uncommented code for Optional class example demonstrating usage of Optional to handle null values.

--- Java8features/src/com/features/OptionalClassExample.java
+++ Java8features/src/com/features/OptionalClassExample.java
@@ -16,3 +16,36 @@ public class OptionalClassExample {
 	 
 	}
 }
+//package com.features;
+
+// import java.util.Optional;
+
+// class User{
+// 	//for printing names without null iterating 
+// 	public static void Name(String[] s) {
+// 		for(String s1:s) {
+// 		    if(s1!=null) {
+// 			  System.out.print(s1+" ");
+// 		    }
+// 		}
+// 	}
+// 	//Optional:used to get the obj  which can hold null or any obj
+// 	//for optional we use method which returns anything null or obj that can store anything
+	
+// 	public static Optional<String> getName(String[] s,int index) {
+// 		return Optional.ofNullable(s[index]);
+// 	}
+// }
+
+
+// public class OptionalReturnExample {
+//     public static void main(String[] args) {
+//     	String[] s= {"deva","datta",null,"wewe"};
+//           Optional<String> s1=User.getName(s, 2);
+//           s1.ifPresentOrElse(
+//         		  (value)->System.out.println(value),
+//         		  ()->System.out.println("user not found")
+//           );     
+//     }
+// }
+
