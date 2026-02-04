391211618c6f7f54f99e9f4571492366acdf517d
Devadatta57 <193812273+Devadatta57@users.noreply.github.com>
2026-02-04 22:17:18 +0530

    Update ReservationTest.java

--- Multithreading/src/ccom/Reservation/ReservationTest.java
+++ Multithreading/src/ccom/Reservation/ReservationTest.java
@@ -14,3 +14,19 @@ public class ReservationTest{
   
 	}
 }
+//Runnable interface is implemented by multiple threads on a shared obj so same lock same shared obj 
+//If we want multiple threads to work on the same shared object, we should create one object and pass it to multiple threads. Implementing the Runnable interface is more recommended than extending the Thread class.
+//“Runnable is preferred because multiple threads can share the same object easily.”
+//In Thread, each thread has its own object, so locks are different,how many thread objts created that many objts created 
+//and have diff locks on diff objts not same lock”
+// Reservation t= new Reservation(2);
+   //    t.setName("deva");
+   //    t.start();
+      
+   //    Reservation t1= new Reservation(2);
+   //    t1.setName("waste");
+   //    t1.start();
+       
+   //    Reservation t2= new Reservation(2);
+   //    t2.setName("fellow");
+	  // t2.start();	
